889401bee8a8948d7e791c4654abc93ca03ad6db
Ravi Bhoraskar <ravibhoraskar@gmail.com>
2012-03-04 23:50:12 +0530

    Changed the reaction of mouse pointer to a quadratic function of acceleration, instead of linear. Gives nicer motion now.

--- movemouse.py
+++ movemouse.py
@@ -2,7 +2,7 @@
 
 import sys,os
 
-scale=2
+scale=1
 
 while(1):
     #line=sys.stdin.readline();
@@ -12,8 +12,8 @@ while(1):
     #print words
     if("accel" in line):
         #print words[-3]+" "+words[-2]+" ";
-        x=float(words[-3])
-        y=float(words[-2])
+        x=1.5*float(words[-3])*abs(float(words[-3]))+float(words[-3])
+        y=1.5*float(words[-2])*abs(float(words[-2]))+float(words[-2])
         os.system("xdotool mousemove_relative -- "+str(scale*y)+" "+str(scale*x));
     elif("leftclick" in line):
         os.system("xdotool click 1");
